f116d95785605cc7aead5621343199c6cda22cf2
João Fernando Apel Miguel <joaoapel@buzzlabs.com.br>
2025-04-11 11:19:59 -0300

    map no parsing das letras

--- parsing.js
+++ parsing.js
@@ -1,34 +1,29 @@
-// Imports the Google Cloud client library
 const language = require('@google-cloud/language').v2;
 
 const musicas = require('./musicas')
 
-// Creates a client
 const client = new language.LanguageServiceClient();
 
-process.env.GOOGLE_APPLICATION_CREDENTIALS='/home/joao/Documentos/chave-api/curious-sandbox-456314-a8-490f7fb376f0.json'
+process.env.GOOGLE_APPLICATION_CREDENTIALS = '/home/joao/Documentos/chave-api/curious-sandbox-456314-a8-490f7fb376f0.json'
 
-const text = Object.values(musicas)[0][0];
+const listaLetras = musicas.musicas.map(musicas => musicas.letra)
 
-console.log(text)
-
-// Prepares a document, representing the provided text
-const document = {
-    content: text.letra,
+async function parseEntidades(letra) {
+  const document = {
+    content: letra,
     type: 'PLAIN_TEXT',
   };
-  
 
-  (async function parseEntidades() {
-    const entidades = await client.analyzeEntities({ document: document });
+  const entidades = await client.analyzeEntities({ document: document });
 
-    const lista = await entidades[0].entities;  
+  const lista = await entidades[0].entities;
 
-    const listaFiltrada = await lista.filter((entidade) => {
-        return entidade.type = 'PERSON'
-    }).map(user => user.name);    
-    
-    console.log(listaFiltrada)
-   
-  })();
+  const listaFiltrada = await lista.filter((entidade) => {
+    return entidade.type = 'PERSON'
+  }).map(user => user.name);
 
+  console.log(listaFiltrada)
+
+}
+
+listaLetras.flatMap(elemento => parseEntidades(elemento))
