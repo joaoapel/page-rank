873fef10b9fe06b03dfa0c182ed70dbc107d61c2
João Fernando Apel Miguel <joaoapel@buzzlabs.com.br>
2025-04-11 12:16:59 -0300

    add try catch e coloquei no object

--- parsing.js
+++ parsing.js
@@ -6,24 +6,26 @@ const client = new language.LanguageServiceClient();
 
 process.env.GOOGLE_APPLICATION_CREDENTIALS = '/home/joao/Documentos/chave-api/curious-sandbox-456314-a8-490f7fb376f0.json'
 
-const listaLetras = musicas.musicas.map(musicas => musicas.letra)
-
 async function parseEntidades(letra) {
-  const document = {
-    content: letra,
-    type: 'PLAIN_TEXT',
-  };
+  try {
+    const document = {
+      content: letra,
+      type: 'PLAIN_TEXT',
+    };
 
-  const entidades = await client.analyzeEntities({ document: document });
+    const entidades = await client.analyzeEntities({ document: document });
 
-  const lista = await entidades[0].entities;
+    const lista = await entidades[0].entities;
 
-  const listaFiltrada = await lista.filter((entidade) => {
-    return entidade.type = 'PERSON'
-  }).map(user => user.name);
+    const listaFiltrada = await lista.filter((entidade) => {
+      return entidade.type = 'PERSON'
+    }).map(user => user.name);
 
-  console.log(listaFiltrada)
+    return console.log(listaFiltrada)
 
+  } catch (error) { console.log('Erro no parsing :/'); }
 }
 
-listaLetras.flatMap(elemento => parseEntidades(elemento))
+const listaParseada = musicas.musicas.map(elemento => [{artista: elemento.artista, entidades: parseEntidades(elemento.letra)}])
+
+console.log(listaParseada)
